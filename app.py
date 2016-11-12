a82ff623a10b9fddd1e11ad23862fd9e24540b0c
rmehta4 <rmehta4@ncsu.edu>
2016-11-12 11:51:56 -0500

    Done

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, redirect,render_template, url_for, session
+from flask import Flask, redirect,render_template, url_for, session ,jsonify , request
 from flask_oauth import OAuth
- 
+from flask_api import status
+from flask_cors import CORS, cross_origin
+import uuid
+import json
+import xlrd
+import numpy as np
+import csv
+import logging
+
+import sqlite3 as sqllite
+import sys 
  
 # You must configure these 3 values from Google APIs console
 # https://code.google.com/apis/console
@@ -16,6 +26,11 @@ app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
  
+
+configTable = {}
+cur = None
+con = None
+
 google = oauth.remote_app('google',
                           base_url='https://www.google.com/accounts/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
@@ -27,9 +42,46 @@ google = oauth.remote_app('google',
                           access_token_params={'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
- 
+
+def setup_sql_lite_db():
+    try:
+
+        global con
+        con = sqllite.connect('identity_json.db', check_same_thread=False)
+        global cur
+        cur = con.cursor()
+        #cur.execute('DROP TABLE IF EXISTS Comment')
+        sql = "CREATE TABLE IF NOT EXISTS Identity (" \
+              "    id VARCHAR, " \
+              "    email TEXT," \
+	      "    verified VARCHAR," \
+              "    hd VARCHAR )"
+        cur.execute(sql)
+        con.commit()
+
+    except sqllite.Error, e:
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+
+
+
+## Json format to return web api calls
+
+notes = {
+    0: 'not verified',
+    1: 'verfied',
+}
+
+def Identity_repr(key):
+    return {
+        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
+        'text': identify[key]
+    }
+
+
 @app.route('/')
 def index():
+    global cur, con
     access_token = session.get('access_token')
     if access_token is None:
         return redirect(url_for('login'))
@@ -48,10 +100,42 @@ def index():
             session.pop('access_token', None)
             return redirect(url_for('login'))
         return res.read()
-    return render_template('developer.html')
-    #return res.read()
+    #return render_template('developer.html')
+    #cur.execute("INSERT INTO Config (id, json ) VALUES('" + str(id) + "', '" + json.dumps(request.json) + "')")
+    #con.commit()
+   
+    data = res.read()
+    json1_data = json.loads(data)
+
+    # If not using ncsu gmail id then redirect [ temporary] 	
+    if 'hd' not in json1_data:
+	return redirect(url_for('login'))
+
+    cur.execute("SELECT verified FROM Identity WHERE id='" + str(json1_data["id"]) + "'")
+    rows = cur.fetchall()
  
+    if(len(rows)==0):
+	insert(json1_data,cur,con)
+    else:
+	return render_template('temp.html', json_data = json1_data["id"])  ## render shortcut one 
+	
+
+
+   # cur.execute("INSERT INTO Identity (id, email ,verified ,hd ) VALUES('" + str(json1_data["id"]) + "', '" + str(json1_data["email"]) +"', '"
+#+ str(json1_data["verified_email"]) +"', '" + str(json1_data["hd"]) +"')") 
+    #con.commit()
+
+
+    return data
  
+
+def insert(json1_data,cur,con):
+    cur.execute("INSERT INTO Identity (id, email ,verified ,hd ) VALUES('" + str(json1_data["id"]) + "', '" + str(json1_data["email"]) +"', '"
++ str(json1_data["verified_email"]) +"', '" + str(json1_data["hd"]) +"')") 
+    con.commit()
+
+
+
 @app.route('/login')
 def login():
     callback=url_for('authorized', _external=True)
@@ -73,6 +157,7 @@ def get_access_token():
  
  
 def main():
+    setup_sql_lite_db()
     app.run()
  
  
