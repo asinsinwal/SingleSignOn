a02465b938b7f9d5f97cffc65761bdc4ff0f6a39
mhpandya <mhpandya@ncsu.edu>
2016-11-20 16:09:27 -0500

    Before encryption

--- RainbowGraphService/RainbowGraphService.py
+++ RainbowGraphService/RainbowGraphService.py
@@ -407,4 +407,4 @@ def visualize(id):
 if __name__ == '__main__':
     setup_sql_lite_db()
     #app.run(host='127.0.0.1', port=3005, threaded=True)
-    app.run(host='0.0.0.0', port=3005, threaded=True)
+    app.run(host='127.0.0.2', port=3005, threaded=True)
