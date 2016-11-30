86ecd5cef18f34bcc47bdb9218436e13a61485bd
mhpandya <mhpandya@ncsu.edu>
2016-11-29 22:38:53 -0500

    Created Sign up functionality before accessing services

--- controller/signup.py
+++ controller/signup.py
@@ -2,5 +2,5 @@ from flask_wtf import Form
 from wtforms import TextField
 
 class SignupForm(Form):
-   name = TextField("Name Of Student")
+   name = TextField("Name")
    email = TextField("Email ID")
